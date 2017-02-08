5a82ca27a60a287cea209aadb46efe293e01732b
francisco15 <franciscohuertas38@gmail.com>
2017-02-08 15:35:27 -0500

    agregada autenticacion

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,9 @@
 class ExpensesController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   def index
-	@expenses = Expense.order("date DESC")
+	@expenses = Expense.order("date DESC").where("user_id=?",current_user.id)
 
 	if params[:concept].present?
 		@expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
@@ -9,6 +12,6 @@ class ExpensesController < ApplicationController
 	if params[:category_id].present?
     	@expenses = @expenses.where("category_id=?", params[:category_id])
     end
-
   end
+  
 end
